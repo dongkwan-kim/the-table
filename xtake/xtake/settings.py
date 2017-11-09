9f07b2464cb5dd8b77060cc24745b640476c7a45
Dongkwan Kim <todoaskit@gmail.com>
2017-11-09 17:41:28 +0900

    Settings for templates, static and host

--- xtake/xtake/settings.py
+++ xtake/xtake/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(rvh029sdv(+20sy!rwfz9vqsctw678@7dcpz)$p4sw9w#g(x$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'xtake.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, "templates"),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS =[
+    os.path.join(BASE_DIR, 'static'),
+]
