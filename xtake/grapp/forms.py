49f505e96ba25b84bfee17dd8bbc523c452be028
Dongkwan Kim <todoaskit@gmail.com>
2017-11-23 15:24:01 +0900

    Make different version of table interface

--- xtake/grapp/forms.py
+++ xtake/grapp/forms.py
@@ -11,6 +11,12 @@ class UserResponseForm(forms.Form):
 
         self.request = request
         self.profile = UserProfile.objects.get(user=request.user)
+
+        self.fields['stake'] = forms.ChoiceField(
+            choices=self.get_stakes(),
+            help_text="이 공약은 당신에게 이익을 주나요, 손해를 주나요?",
+            widget=forms.RadioSelect()
+        )
         self.fields['demographics'] = forms.MultipleChoiceField(
             required=False,
             widget=forms.CheckboxSelectMultiple,
@@ -30,6 +36,13 @@ class UserResponseForm(forms.Form):
             widget=forms.HiddenInput(),
         )
 
+    def get_stakes(self):
+        return (
+            (1, '이익을 준다'),
+            (-1, '손해를 준다'),
+            (0, '관계 없다'),
+        )
+
     def get_demographics(self):
         fields = [(f.verbose_name, f.name, self.profile.get_value(f.name)) \
                     for f in UserProfile._meta.fields \
@@ -39,5 +52,4 @@ class UserResponseForm(forms.Form):
     def get_binary_questions(self):
         fields = self.profile.get_value('answers')
         return fields
-
 
